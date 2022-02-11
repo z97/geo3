801c8648f0df2177e462eaf5d1b976509104b2d9
kliba <klibaner@mail.com>
2022-02-11 19:49:22 +0200

    master

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,5 @@
-mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicTEyMiIsImEiOiJja3ppaHRvMjMwOHBuMndsNHNrOWIzMHowIn0.meHB0exDESSG7DVK5ee9Uw';
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
